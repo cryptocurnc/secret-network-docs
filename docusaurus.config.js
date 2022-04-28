c247e1973d95395ec1b82f077cac091224cef3c2
cryptocurnc <7717686+cryptocurnc@users.noreply.github.com>
2022-04-28 14:19:56 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cryptocurnc', // Usually your GitHub org/user name.
+  projectName: 'secret-network-docs', // Usually your repo name.
 
   presets: [
     [
